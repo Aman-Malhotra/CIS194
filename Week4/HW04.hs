c341be224ce7cd1fc6a9567d6988882fa3e61082
Aman-Malhotra <official.aman18@gmail.com>
2021-01-24 14:38:28 +0530

    small fix + new ex

--- Week4/HW04.hs
+++ Week4/HW04.hs
@@ -30,14 +30,17 @@ showHelper (P (p:ps)) e c
     | p == 0                = showHelper (P ps) (e-1) (c+1)
     | e == 0                = leadingPlus ++ show p 
     -- I was trying to get nested guards here instead of using [if-then-else] or [case] but wasnt able to 
-    | e == 1                = (case p of 
-        0                   -> ""
-        1                   -> leadingPlus ++ "x"
-        anyP                -> leadingPlus  ++ show anyP ++ "x" )  ++ showHelper (P ps) (e-1) (c+1)
-    | otherwise             = leadingPlus ++ show p ++ "x^" ++ show e ++ showHelper (P ps) (e-1) (c+1)
-    where leadingPlus       = case c of 
+    | e == 1                = leadingNum ++ "x"  ++ showHelper (P ps) (e-1) (c+1)
+    | otherwise             = leadingPlus ++ leadingNum ++ "x^" ++ show e ++ showHelper (P ps) (e-1) (c+1)
+    where 
+        leadingPlus         = case c of 
                 0           -> "" 
                 _           -> " + "
+        leadingNum          = case p of 
+                1           -> ""
+                anyP        -> show anyP
+
+
 showHelper (P []) _ _       = ""
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
@@ -64,38 +67,48 @@ plus (P polyA) (P polyB)    = P $ zipWith (+) (polyA ++ replicate leadingZerosTo
 
 -- c is the coeff being multiplied from the first polynomial
 -- e is the exponentioal term being used for multiplication from first polynomial
-timesHelper ::  (Num a)     => Poly a -> a -> Int -> Poly a
-timesHelper (P p) c e = P $ replicate e 0 ++ map (*c) p
 
+-- timesHelper (P [1, 4, 2]) 1 0 == P [1, 4, 2]
+-- timesHelper (P [1, 4, 2]) 1 1 == P [0, 1, 4, 2]
+-- timesHelper (P [1, 4, 2]) 1 2 == P [0, 0, 1, 4, 2]
+-- timesHelper (P [1, 4, 2]) 2 2 == P [0, 0, 2, 8, 4]
+
+-- 1. add leading zeros list of size == e 
+-- and multiply rest with c
+timesHelper ::  (Num a)     => Poly a -> a -> Int -> Poly a
+timesHelper (P p) c e       = P $ replicate e 0 ++ map (*c) p
+
+-- getAllTimes 0 (P [1, 1, 2]) (P [1, 4, 2]) == [P [1, 4, 2], P [0, 1, 4, 2], P [0, 0, 2, 8, 4]]
+getAllTimes ::  (Num a)     => Int -> Poly a -> Poly a -> [Poly a]
+getAllTimes _ (P []) _      = []
+getAllTimes n (P [p]) q     = [timesHelper q p n]
+getAllTimes n (P (p:ps)) q  = timesHelper q p n :  getAllTimes (n+1) (P ps) q
+
+-- addAllTimes [P [1, 4, 2], P [0, 1, 4, 2], P [0, 0, 2, 8, 4]] == P [1, 5, 8, 10, 4]
+-- This is how it works with the help of [timesHelper] function
+--      [1, 4, 2, 0, 0]
+--   +  [0, 1, 4, 2, 0]
+--   +  [0, 0, 2, 8, 4]
+--      ---------------
+--      [1, 5, 8, 10, 4]
+--      ---------------
 addAllTimes :: (Num a)      => [Poly a] -> Poly a
 addAllTimes []              = P []
 addAllTimes [p1]            = p1
 addAllTimes [p1, p2]        = plus p1 p2
 addAllTimes (p1:p2:ps)      = plus p1 p2 + addAllTimes ps
 
-getAllTimes ::  (Num a)     => Int -> Poly a -> Poly a -> [Poly a]
-getAllTimes _ (P []) _      = []
-getAllTimes n (P [p]) q     = [timesHelper q p n]
-getAllTimes n (P (p:ps)) q  = timesHelper q p n :  getAllTimes (n+1) (P ps) q
-
 times :: (Num a)            => Poly a -> Poly a -> Poly a
 times p q                   = addAllTimes $ getAllTimes 0 p q
 
--- in progress
--- times (P a) (P b) = result 
---     where 
---         allTimes :: [Poly a]
---         allTimes = undefined 
---         result   = addAllTimes allTimes
-        
-
 -- Exercise 6 -----------------------------------------
 
-instance Num a => Num (Poly a) where
-    (+) = plus
-    (*) = times
-    negate      = undefined
-    fromInteger = undefined
+instance Num a             => Num (Poly a) where
+    (+)                    = plus
+    (*)                    = times
+    negate                 = (* P [-1])
+    fromInteger i          = P [fromInteger i]
+
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
